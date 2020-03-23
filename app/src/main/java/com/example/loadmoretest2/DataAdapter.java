b8481a51c1d689c92eae6981f4739e8f5ace739b
LulaKao <windandsprings@gmail.com>
2020-03-23 13:48:18 +0800

    branch test 3

--- app/src/main/java/com/example/loadmoretest2/DataAdapter.java
+++ app/src/main/java/com/example/loadmoretest2/DataAdapter.java
@@ -6,11 +6,8 @@ import android.view.ViewGroup;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import java.util.List;
 
 public class DataAdapter extends RecyclerView.Adapter {
